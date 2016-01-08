cd0c9091a553a4c60169c59313760b27d2a2f774
noyobo <nongyoubao@alibaba-inc.com>
2016-01-08 17:17:50 +0800

    test file

---
+++ test/gulpfile.js
@@ -0,0 +1,15 @@
+
+var gulp = require('gulp');
+
+var concatStyle = require('../');
+
+
+gulp.task('default', function() {
+  return gulp
+  .src('./retina/*.less')
+  .pipe(concatStyle({
+    entry: '<filename>-<id>.less',
+    output: '[filename].less'
+  }))
+  .pipe(gulp.dest('./build'))
+})
